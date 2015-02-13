de2c8da3551d7a64b0b1147637b65bedeac4d978
Edmond Chui <chui.hinwai@gmail.com>
2015-02-13 02:46:43 -0700

    removed unused methods

--- src/com/edmondapps/cs301/ass1/model/ClaimUtils.java
+++ src/com/edmondapps/cs301/ass1/model/ClaimUtils.java
@@ -16,12 +16,6 @@
 
 package com.edmondapps.cs301.ass1.model;
 
-import org.joda.money.CurrencyMismatchException;
-import org.joda.money.CurrencyUnit;
-import org.joda.money.Money;
-
-import java.util.Iterator;
-
 public final class ClaimUtils {
     private ClaimUtils() {
     }
@@ -42,49 +36,5 @@ public final class ClaimUtils {
         if (number < 0) {
             throw new IllegalArgumentException(name + " must not be negative.");
         }
-    }
-
-    public static void nonNegativeOrThrow(Money money, String name) {
-        if (money.isNegative()) {
-            throw new IllegalArgumentException(name + " must not be negative.");
-        }
-    }
-
-    public static void sameCurrencyOrThrow(Money first, Money second) {
-        if (!first.isSameCurrency(second)) {
-            throw new CurrencyMismatchException(first.getCurrencyUnit(), second.getCurrencyUnit());
-        }
-    }
-
-    public static void sameCurrencyOrThrow(CurrencyUnit unit, Money money) {
-        if (!unit.equals(money.getCurrencyUnit())) {
-            throw new CurrencyMismatchException(unit, money.getCurrencyUnit());
-        }
-    }
-
-    public static Iterable<Money> mapMoneyFromExpense(final Iterable<Expense> expenses) {
-        return new Iterable<Money>() {
-            @Override
-            public Iterator<Money> iterator() {
-                return new Iterator<Money>() {
-                    private Iterator<Expense> mIterator = expenses.iterator();
-
-                    @Override
-                    public boolean hasNext() {
-                        return mIterator.hasNext();
-                    }
-
-                    @Override
-                    public Money next() {
-                        return mIterator.next().getAmount();
-                    }
-
-                    @Override
-                    public void remove() {
-                        mIterator.remove();
-                    }
-                };
-            }
-        };
     }
 }

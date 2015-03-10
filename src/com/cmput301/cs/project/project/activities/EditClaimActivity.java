1d103db225e7603ea67cb6fd8523ff7fd8d94ad2
Edmond Chui <chui.hinwai@gmail.com>
2015-03-10 14:50:04 -0600

    removed generated stuff

--- src/com/cmput301/cs/project/project/activities/EditClaimActivity.java
+++ src/com/cmput301/cs/project/project/activities/EditClaimActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import com.cmput301.cs.project.R;
@@ -124,25 +122,5 @@ public class EditClaimActivity extends Activity {
         if (mBuilder.isEndTimeSet()) {
             mEndDateButt.setText(mDateFormat.format(mBuilder.getEndTime()));
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.new_claim, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }

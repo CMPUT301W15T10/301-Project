f7d89305c633bf0da2e52e53470f7fe0864ec830
Edmond Chui <chui.hinwai@gmail.com>
2015-03-13 18:37:49 -0600

    implements Parcelable for User (fixes exception while parceling Claim)

--- src/com/cmput301/cs/project/model/User.java
+++ src/com/cmput301/cs/project/model/User.java
@@ -1,13 +1,16 @@
 package com.cmput301.cs.project.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.UUID;
 
-/** 
+/**
  * This class allows for the creation of and association of a username and user id to a user that will allow
  *  the user to log into the application and use it. 
  */
 
-public class User {
+public class User implements Parcelable {
 
     private final String name;
     private final UUID userId;
@@ -54,4 +57,33 @@ public class User {
 
         return result;
     }
+
+    protected User(Parcel in) {
+        name = in.readString();
+        userId = (UUID) in.readValue(UUID.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeValue(userId);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }

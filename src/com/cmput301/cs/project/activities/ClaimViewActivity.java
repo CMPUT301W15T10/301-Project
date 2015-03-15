185584b3a39db4fdb8eada98a7ea49b176a52d57
ChuckRozsa <chuckeroz@gmail.com>
2015-03-15 16:50:03 -0600

    Added menu buttons to ClaimViewActivity, edit works, delete doesn't. Made it so ClaimViewActivity has the dates of the Claim.

--- src/com/cmput301/cs/project/activities/ClaimViewActivity.java
+++ src/com/cmput301/cs/project/activities/ClaimViewActivity.java
@@ -7,10 +7,12 @@ import com.cmput301.cs.project.model.Claim;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import java.text.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ClaimViewActivity extends Activity {
 	
@@ -18,6 +20,9 @@ public class ClaimViewActivity extends Activity {
 	Claim claim;
 	Button mExpenseButton;
 	Button mSubmitButton;
+	TextView mStartDate;
+	TextView mEndDate;
+	DateFormat mDateFormat;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,13 @@ public class ClaimViewActivity extends Activity {
 		claim = getIntent().getExtras().getParcelable(App.KEY_CLAIM);
 		mExpenseButton = (Button) findViewById(R.id.expenseButton);
 		mSubmitButton = (Button) findViewById(R.id.submitButton);
+		mStartDate = (TextView) findViewById(R.id.startDate);
+		mEndDate = (TextView) findViewById(R.id.endDate);
+		mDateFormat = android.text.format.DateFormat.getMediumDateFormat(this);
+		mStartDate.setText(mDateFormat.format(claim.getStartTime()));
+		mEndDate.setText(mDateFormat.format(claim.getEndTime()));
+		
+	
 		
 		initButtons();
 				
@@ -64,10 +76,18 @@ public class ClaimViewActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+			case R.id.deleteClaim:
+				
+			case R.id.editClaim:
+				Intent intent = new Intent(ClaimViewActivity.this, EditClaimActivity.class);
+				intent.putExtra(App.KEY_CLAIM, claim);
+				startActivity(intent);
+				
+			default:
+				return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+		
+		
 	}
 }

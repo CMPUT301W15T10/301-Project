16fc556b484485952c9590c64381b7174fcfb23f
Diusrex <killakan002@hotmail.com>
2015-03-15 16:10:38 -0600

    Lots of improvements to EditExpenseActivity

--- src/com/cmput301/cs/project/activities/EditExpenseActivity.java
+++ src/com/cmput301/cs/project/activities/EditExpenseActivity.java
@@ -10,15 +10,16 @@ import com.cmput301.cs.project.R;
 import com.cmput301.cs.project.TextWatcherAdapter;
 import com.cmput301.cs.project.model.Expense;
 import com.cmput301.cs.project.utils.Utils;
-
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.text.DateFormat;
 
 public class EditExpenseActivity extends Activity {
     public static final String KEY_EXPENSE = "key_expense";
+    private static final int REQ_CODE_PICK_DATE = 1;
+
     private Expense.Builder mBuilder;
 
     private EditText mDescription;
@@ -26,14 +27,27 @@ public class EditExpenseActivity extends Activity {
 
     private Spinner mCategory;
     private Spinner mCurrency;
-    private Button mDate;
 
     private Switch mCompleted;
+
+    private Button mDate;
+    private DateFormat mDateFormat;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_edit_activity);
-        
+
+        createDiscardDoneBar();
+
+        mDateFormat = android.text.format.DateFormat.getMediumDateFormat(this);
+
+        createBuilder();
+
+        initEditing();
+        updateUI();
+    }
+
+    private void createDiscardDoneBar() {
         Utils.setupDiscardDoneBar(this, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,26 +56,23 @@ public class EditExpenseActivity extends Activity {
         }, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              
+
                 finish();
             }
         });
-
-        createBuilder();
-
-        initEditing();
-        updateDisplay();
     }
 
     private void createBuilder() {
         Expense expense = getIntent().getParcelableExtra(KEY_EXPENSE);
-
         if (expense == null)
             mBuilder = new Expense.Builder();
         else
             mBuilder = Expense.Builder.copyFrom(expense);
     }
 
+    /*
+     * Method that initializes all of the editing related members, and sets up their responses to input.
+     */
     private void initEditing() {
         mDescription = (EditText) findViewById(R.id.description);
         mDescription.addTextChangedListener(new TextWatcherAdapter() {
@@ -99,7 +110,7 @@ public class EditExpenseActivity extends Activity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-        
+
 
         mCurrency = (Spinner) findViewById(R.id.currency);
         mCurrency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -122,16 +133,62 @@ public class EditExpenseActivity extends Activity {
                 mBuilder.completed(isChecked);
             }
         });
+
+        mDate = (Button) findViewById(R.id.date);
+        mDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new CalendarActivity.Builder(EditExpenseActivity.this)
+                        .selectedDate(mBuilder.getTime()).build(), REQ_CODE_PICK_DATE);
+            }
+        });
     }
 
-    private void updateDisplay() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQ_CODE_PICK_DATE:
+                if (resultCode == RESULT_OK) {
+                    mBuilder.time(data.getLongExtra(CalendarActivity.KEY_DATE, -1));
+                    updateUI();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void updateUI() {
         mDescription.setText(mBuilder.getDescription());
 
         final Money money = mBuilder.getMoney();
         mAmount.setText(money.getAmount().toString());
 
-        //int currentCategoryPosition = mCategory.getAdapter().
-        	//mCategory.setSelection();
+        if (mBuilder.isTimeSet()) {
+            mDate.setText(mDateFormat.format(mBuilder.getTime()));
+        }
+
+        if (mBuilder.isCategorySet()) {
+            int spinnerPosition = getIndex(mCategory, mBuilder.getCategory());
+            mCategory.setSelection(spinnerPosition);
+        }
+
+        int spinnerPosition = getIndex(mCurrency, money.getCurrencyUnit().toString());
+        mCurrency.setSelection(spinnerPosition);
+
+        mCompleted.setChecked(mBuilder.isCompleted());
     }
 
+    //private method of your class
+    private int getIndex(Spinner spinner, String wantedString) {
+        int index = 0;
+
+        for (int i = 0; i < spinner.getCount(); i++) {
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(wantedString)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
 }

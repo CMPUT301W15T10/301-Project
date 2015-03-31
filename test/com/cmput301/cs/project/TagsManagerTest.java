1f5970648d1bd9cab8901c1b7aac1d0d7cf097e3
Edmond Chui <chui.hinwai@gmail.com>
2015-03-31 13:35:12 -0600

    removed irrelevant tests

--- test/com/cmput301/cs/project/TagsManagerTest.java
+++ test/com/cmput301/cs/project/TagsManagerTest.java
@@ -1,6 +1,5 @@
 package com.cmput301.cs.project;
 
-import com.cmput301.cs.project.controllers.TagsChangedListener;
 import com.cmput301.cs.project.controllers.TagsManager;
 import com.cmput301.cs.project.models.Tag;
 import com.cmput301.cs.project.utils.MockClaimSaves;
@@ -12,30 +11,9 @@ import static org.junit.Assert.*;
 public class TagsManagerTest {
 
     private TagsManager mManager;
-    private TagsChangedListener mListener;
-    private Tag mDeletedTag;
-    private Tag mCreatedTag;
 
     @Before
     public void setup() {
-        mDeletedTag = null;
-        mCreatedTag = null;
-        mListener = new TagsChangedListener() {
-            @Override
-            public void onTagRenamed(Tag tag, Tag oldTag) {
-                assertEquals("ok", oldTag.getName());
-            }
-
-            @Override
-            public void onTagDeleted(Tag tag) {
-                assertTrue(mDeletedTag == tag);  // identity check
-            }
-
-            @Override
-            public void onTagCreated(Tag tag) {
-                mCreatedTag = tag;
-            }
-        };
         mManager = TagsManager.ofClaimSaves(new MockClaimSaves());
     }
 
@@ -54,27 +32,5 @@ public class TagsManagerTest {
         assertNotNull(mManager.findTagByName("ok"));
         mManager.deleteTagByName("ok");
         assertNull(mManager.findTagByName("ok"));
-    }
-
-    @Test
-    public void deleteListener() {
-        final Tag okTag = mManager.getTagByName("ok");
-        mDeletedTag = okTag;
-        mManager.addTagChangedListener(mListener);
-        mManager.deleteTagById(okTag.getId());
-    }
-
-    @Test
-    public void createListener() {
-        mManager.addTagChangedListener(mListener);
-        final Tag tag = mManager.getTagByName("ok");
-        assertTrue(mCreatedTag == tag);  // identity check
-    }
-
-    @Test
-    public void renameListener() {
-        final Tag ok = mManager.getTagByName("ok");
-        mManager.addTagChangedListener(mListener);
-        mManager.renameTag(ok, "wut now");
     }
 }

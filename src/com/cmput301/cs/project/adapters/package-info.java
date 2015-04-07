42f4e7f02184c9ffc16034805989e8f3fcf662cc
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-04-07 14:25:34 -0600

    package javadoc

---
+++ src/com/cmput301/cs/project/adapters/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A series of custom list adapters for our Models.
+ */
+
+package com.cmput301.cs.project.adapters;
--- src/com/cmput301/cs/project/models/Saveable.java
+++ src/com/cmput301/cs/project/models/Saveable.java
@@ -1,6 +1,7 @@
 package com.cmput301.cs.project.models;
 
 /**
+ * This interface provides an ID for elastic search to index the given class.
  */
 
 public interface Saveable {
